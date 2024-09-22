2e00ad52bcbe86c7d4ffc00556adb7cc4feaa072
ohmslot <ohmmho47@gmail.com>
2024-09-22 08:21:36 +0700

    edit event

--- admin/ajax/hotel_facilities.php
+++ admin/ajax/hotel_facilities.php
@@ -49,8 +49,14 @@
                     <td>$row[time_e]</td>
                     <td>$row[description]</td>
                     <td>
-                        <button type="button" onclick="rem_facility($row[id])" class="btn btn-danger btn-sm shadow-none">
-                            <i class="bi bi-trash"></i> Delete
+                        <button type="button" onclick="edit_facility_info($row[id],'$row[name]','$row[time_s]','$row[time_e]','$row[description]')" class="btn btn-secondary btn-md shadow-none">
+                            <i class="bi bi-pencil-square"></i>
+                        </button>
+                        <button type="button" onclick="edit_facility_image($row[id],'$path$row[image]')" class="btn btn-success btn-md shadow-none">
+                            <i class="bi bi-image"></i>
+                        </button>
+                        <button type="button" onclick="rem_facility($row[id])" class="btn btn-danger btn-md shadow-none">
+                            <i class="bi bi-trash"></i>
                         </button>
                     </td>
                 </tr>
@@ -58,6 +64,46 @@
             $i++;
         }
     }
+
+    if(isset($_POST['update_facility_info']))
+    {
+        $frm_data = filteration($_POST);
+        $q = "UPDATE `hotel_facilities` SET `name`=?, `time_s`=?, `time_e`=?, `description`=? WHERE `id`=?";
+        $values = [$frm_data['name'], $frm_data['time_s'], $frm_data['time_e'], $frm_data['desc'], $frm_data['id']];
+        $res = update($q, $values, 'ssssi');
+        echo $res;
+    }
+
+    if(isset($_POST['update_facility_image']))
+    {
+        $frm_data = filteration($_POST);
+        
+        $img_r = uploadImage($_FILES['image'], HOTEL_FACILITIES_FOLDER);
+
+        if($img_r == 'inv_img'){
+            echo $img_r;
+        }
+        else if($img_r == 'inv_size'){
+            echo $img_r;
+        }
+        else if($img_r == 'upd_failed'){
+            echo $img_r;
+        }
+        else{
+            // ลบรูปภาพเก่า
+            $pre_q = "SELECT * FROM `hotel_facilities` WHERE `id`=?";
+            $res = select($pre_q,[$frm_data['id']],'i');
+            $img = mysqli_fetch_assoc($res);
+            deleteImage($img['image'],HOTEL_FACILITIES_FOLDER);
+
+            // อัปเดตรูปภาพใหม่
+            $q = "UPDATE `hotel_facilities` SET `image`=? WHERE `id`=?";
+            $values = [$img_r, $frm_data['id']];
+            $res = update($q, $values, 'si');
+            echo $res;
+        }
+    }
+
 
     if(isset($_POST['rem_facility']))
     {
--- admin/hotel_facilities.php
+++ admin/hotel_facilities.php
@@ -96,6 +96,72 @@ adminLogin();
         </div>
     </div>
 
+    <!-- Edit Facility Info Modal -->
+    <div class="modal fade" id="edit-facility-info" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <form id="edit_facility_info_form">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Facility Info</h1>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" name="facility_id">
+                        <div class="mb-3">
+                            <label class="form-label">Name</label>
+                            <input type="text" name="facility_name" class="form-control shadow-none">
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-6">
+                                <label class="form-label">Time open</label>
+                                <input type="time" name="facility_time_s" class="form-control shadow-none">
+                            </div>
+                            <div class="col-6">
+                                <label class="form-label">Time close</label>
+                                <input type="time" name="facility_time_e" class="form-control shadow-none">
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Description</label>
+                            <textarea name="facility_desc" class="form-control shadow-none" rows="5"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <!-- Edit Facility Image Modal -->
+    <div class="modal fade" id="edit-facility-image" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <form id="edit_facility_image_form">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Facility Image</h1>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3 text-center">
+                            <img id="current_facility_image" src="" alt="Current Image" class="img-thumbnail" width="350px">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">New Image</label>
+                            <input type="file" name="facility_image" class="form-control shadow-none">
+                            <input type="hidden" name="facility_id">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
     <?php include 'inc/script.php'; ?>
     <script>
         let hotel_facilities_form = document.getElementById("hotel_facilities_form");
@@ -148,6 +214,94 @@ adminLogin();
 
             xhr.send("get_facility");
         }
+
+        function edit_facility_info(id, name, time_s, time_e, desc) {
+            let myModal = new bootstrap.Modal(document.getElementById('edit-facility-info'));
+            document.getElementById('edit_facility_info_form').elements['facility_id'].value = id;
+            document.getElementById('edit_facility_info_form').elements['facility_name'].value = name;
+            document.getElementById('edit_facility_info_form').elements['facility_time_s'].value = time_s;
+            document.getElementById('edit_facility_info_form').elements['facility_time_e'].value = time_e;
+            document.getElementById('edit_facility_info_form').elements['facility_desc'].value = desc;
+            myModal.show();
+        }
+
+        function edit_facility_image(id, currentImagePath) {
+            let myModal = new bootstrap.Modal(document.getElementById('edit-facility-image'));
+            document.getElementById('current_facility_image').src = currentImagePath;
+            document.getElementById('edit_facility_image_form').elements['facility_id'].value = id;
+            myModal.show();
+        }
+
+        let edit_facility_info_form = document.getElementById("edit_facility_info_form");
+        let edit_facility_image_form = document.getElementById("edit_facility_image_form");
+
+        edit_facility_info_form.addEventListener("submit", function(e) {
+            e.preventDefault();
+            update_facility_info();
+        });
+
+        edit_facility_image_form.addEventListener("submit", function(e) {
+            e.preventDefault();
+            update_facility_image();
+        });
+
+        function update_facility_info() {
+            let data = new FormData();
+            data.append("id", edit_facility_info_form.elements["facility_id"].value);
+            data.append("name", edit_facility_info_form.elements["facility_name"].value);
+            data.append("time_s", edit_facility_info_form.elements["facility_time_s"].value);
+            data.append("time_e", edit_facility_info_form.elements["facility_time_e"].value);
+            data.append("desc", edit_facility_info_form.elements["facility_desc"].value);
+            data.append("update_facility_info", "");
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/hotel_facilities.php", true);
+
+            xhr.onload = function() {
+                var myModal = document.getElementById("edit-facility-info");
+                var modal = bootstrap.Modal.getInstance(myModal);
+                modal.hide();
+
+                if (this.responseText == 1) {
+                    alert("success", "Facility information updated!");
+                    edit_facility_info_form.reset();
+                    get_facility();
+                } else {
+                    alert("error", "Server down!");
+                }
+            };
+            xhr.send(data);
+        }
+
+        function update_facility_image() {
+            let data = new FormData();
+            data.append("id", edit_facility_image_form.elements["facility_id"].value);
+            data.append("image", edit_facility_image_form.elements["facility_image"].files[0]);
+            data.append("update_facility_image", "");
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "ajax/hotel_facilities.php", true);
+
+            xhr.onload = function() {
+                var myModal = document.getElementById("edit-facility-image");
+                var modal = bootstrap.Modal.getInstance(myModal);
+                modal.hide();
+
+                if (this.responseText == "inv_img") {
+                    alert("error", "Only JPG and PNG image are allowed!");
+                } else if (this.responseText == "inv_size") {
+                    alert("error", "Image should be less than 2MB!");
+                } else if (this.responseText == "upd_failed") {
+                    alert("error", "Image upload failed. Server Down!");
+                } else {
+                    alert("success", "Facility image updated!");
+                    edit_facility_image_form.reset();
+                    get_facility();
+                }
+            };
+            xhr.send(data);
+        }
+
 
         function rem_facility(val) {
             let xhr = new XMLHttpRequest();
